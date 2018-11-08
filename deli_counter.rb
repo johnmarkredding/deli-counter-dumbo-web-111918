2b0dd532bb0a1b37349707db53e305dbf1885aea
John Mark Redding <johnmarkredding@gmail.com>
2018-11-08 21:21:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def line(arr)
+  if arr == []
+    puts "The line is currently empty."
+  else
+    line_str = ""
+    arr.each_with_index {|x, i|
+      line_str += " #{i+1}. #{x}"
+    }
+    puts "The line is currently:" + line_str
+  end
+end
+
+def take_a_number(arr, customer)
+  arr
+end
